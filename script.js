3e7f1b48e22990fbcd0422968f2d2b050957080c
Adonis Mateo <amatt2000@hotmail.com>
2017-03-01 13:27:01 +0000

    I made a house

---
+++ script.js
@@ -0,0 +1,20 @@
+ TinyTurtle.apply(window); 
+ 
+ 
+forward(30);
+left(90);
+forward(30);
+right(115);
+forward(30);
+right(123);
+forward(34);
+right(34);
+forward(30);
+right(90);
+forward(30);
+right(90);
+forward(30);
+
+// penUp();
+// forward(35);
+// penDown();
